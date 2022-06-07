f2d11a7d1c4bc65c238dd29c29ca8e8901ab63ea
matthias.scheer <matthias.scheer@smartinsurtech.de>
2022-06-07 13:36:56 +0200

    change link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ln -sf ./i3 ~/.config/i3
-ln -sf ./i3status-rust ~/.config/i3status-rust
-ln -sf ./i3wsr ~/.config/i3wsr
+mkdir -p ~/.config/i3 && mkdir -p ~/.config/i3wsr && mkdir -p ~/.config/i3status-rust
+
+ln -sf /home/matthias/repositories/local/dotfiles/i3/config ~/.config/i3/config
+ln -sf /home/matthias/repositories/local/dotfiles/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
+ln -sf /home/matthias/repositories/local/dotfiles/i3wsr/config.toml ~/.config/i3wsr/config.toml
